bf95766a2fa658742100a0cd34b3bc5f197b39dd
Laboratório Informática <labinfo@unifil.br>
2019-09-10 22:08:32 -0300

    10092019

--- agenda_db/src/Controller/ServiceDB.java
+++ agenda_db/src/Controller/ServiceDB.java
@@ -1,28 +1,30 @@
 package Controller;
 
 import Models.Contact;
+import Models.Groups;
+import Models.Phones;
+
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ServiceDB {
 
-    public static void insertTables(Connection conexao, String query) {
+    public static int insertTables(Connection conexao, String query) {
+        int id = 0;
         try {
-            Statement stmt = conexao.createStatement();
-            stmt.execute(query);
+            PreparedStatement ps = conexao.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+            ps.executeUpdate();
+            ResultSet rs = ps.getGeneratedKeys();
+            if (rs.next()) {
+                id = rs.getInt(1);
+            }
+            System.out.println("Id gerado pelo insert foi " + id);
         } catch (
                 SQLException e) {
             e.printStackTrace();
-        } finally {
-            try {
-                if (conexao != null) {
-                    conexao.close();
-                }
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
         }
+        return id;
     }
 
     public static String insertContact(String firstName, String lastName, String email) {
@@ -30,11 +32,19 @@ public class ServiceDB {
     }
 
     public static String insertPhone(String phone) {
-        return "insert into phones(phone) values ('" + phone + ")";
+        return "insert into phones(phone) values ('" + phone + "')";
+    }
+
+    public static String insertContactPhone(int contactId, int phoneId) {
+        return "insert into contact_phone(contact_id, phone_id) values ('" + contactId + "', '" + phoneId + "')";
     }
 
     public static String insertGroup(String name) {
         return "insert into groups(name) values ('" + name + ")";
+    }
+
+    public static String insertGroupsContact(int group_id, int contactId) {
+        return "insert into contact_groups(group_id , contact_id) values ('" + group_id + "',  '" + contactId + "')";
     }
 
     public static String getAllcontacts() {
@@ -49,13 +59,15 @@ public class ServiceDB {
         return "select * from contacts where name " + " = " + name;
     }
 
-    public static String selectDB(String query) {
-        Connection conn = null;
-        try {
-            conn = DriverManager.getConnection("jdbc:sqlite:db:sqlite");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+    public static String getAllGroups() {
+        return "select * from groups";
+    }
+
+    public static String getPhoneById(int id) {
+        return "select * from phones where phone_id " + " = " + id;
+    }
+
+    public static List<Contact> selectDBContact(String query, Connection conn) {
         List<Contact> list = new ArrayList<>();
         try (Statement statement = conn.createStatement();
              ResultSet resultSet = statement.executeQuery(query)) {
@@ -65,11 +77,38 @@ public class ServiceDB {
                         resultSet.getString("last_name"),
                         resultSet.getString("email")));
             }
-            conn.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        System.out.println(list.toString());
-        return "result";
+        return list;
     }
+
+    public static List<Phones> selectDBPhones(String query, Connection conn) {
+        List<Phones> list = new ArrayList<>();
+        try (Statement statement = conn.createStatement();
+             ResultSet resultSet = statement.executeQuery(query)) {
+            while (resultSet.next()) {
+                list.add(new Phones(resultSet.getInt("phone_id"),
+                        resultSet.getString("phone")));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
+    public static List<Groups> selectDBCGroups(String query, Connection conn) {
+        List<Groups> list = new ArrayList<>();
+        try (Statement statement = conn.createStatement();
+             ResultSet resultSet = statement.executeQuery(query)) {
+            while (resultSet.next()) {
+                list.add(new Groups(resultSet.getInt("group_id"),
+                        resultSet.getString("name")));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
 }
--- agenda_db/src/View/Main.java
+++ agenda_db/src/View/Main.java
@@ -2,7 +2,11 @@ package View;
 
 import Controller.ConnectionDB;
 import Controller.ServiceDB;
+import Models.Phones;
+
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 import static Controller.ServiceDB.*;
@@ -21,18 +25,49 @@ public class Main {
             opcao = scanner.nextInt();
             switch (opcao) {
                 case 1:
+                    List<Phones> telefones = new ArrayList<>();
                     System.out.println("Digite o nome :");
                     String firstNome = scanner.next();
                     System.out.println("Digite o sobrenome :");
                     String lastName = scanner.next();
                     System.out.println("Digite o email :");
                     String email = scanner.next();
-                    ServiceDB.insertTables(connection, insertContact(firstNome, lastName, email));
+                    int id_contact = ServiceDB.insertTables(connection, insertContact(firstNome, lastName, email));
+                    int aux;
+                    do {
+                        System.out.println("Digite o telefone :");
+                        String telefoneAdd = scanner.next();
+                        int id_telefone = ServiceDB.insertTables(connection, ServiceDB.insertPhone(telefoneAdd));
+                        ServiceDB.insertTables(connection, ServiceDB.insertContactPhone(id_contact, id_telefone));
+                        System.out.println("1 - Cadastrar mais telefone");
+                        System.out.println("2 - Prosseguir");
+                        aux = scanner.nextInt();
+                    } while (aux == 1);
+                    System.out.println("1 - Vincular a grupo existente");
+                    System.out.println("2 - Cadastrar novo grupo");
+                    System.out.println("3 - Finalizar sem grupo");
+                    aux = scanner.nextInt();
+                    if (aux == 1) {
+                        do {
+                            System.out.println(selectDBContact(getAllGroups(), connection).toString());
+                            System.out.println("Digiti os id dos groups");
+                            int id_group = scanner.nextInt();
+                            ServiceDB.insertTables(connection, ServiceDB.insertContactPhone(id_group, id_contact));
+                            aux = scanner.nextInt();
+                            System.out.println("1 - Vincular a mais Grupo");
+                            System.out.println("2 - Finalizar");
+                        } while (aux == 1);
+
+                    }
+                    if(aux == 2){
+                        System.out.println("Digit o nome do grupo: ");
+                        String nome_grupo = scanner.next();
+                        ServiceDB.insertTables(connection, ServiceDB.insertGroup(nome_grupo));
+                    }
+                    System.out.println("Contato cadastrado com sucesso!");
+                    System.out.println(ServiceDB.selectDBContact(ServiceDB.getContactbyId(id_contact), connection));
                     break;
                 case 2:
-                    System.out.println("Digite o telefone :");
-                    String telefone = scanner.next();
-                    ServiceDB.insertTables(connection, ServiceDB.insertPhone(telefone));
                     break;
                 case 3:
                     System.out.println("Digite o nome do grupo :");
@@ -40,17 +75,17 @@ public class Main {
                     ServiceDB.insertTables(connection, ServiceDB.insertGroup(grupoName));
                     break;
                 case 4:
-                    System.out.println(ServiceDB.selectDB(getAllcontacts()));
+                    System.out.println(ServiceDB.selectDBContact(ServiceDB.getAllcontacts(), connection).toString());
                     break;
                 case 5:
                     System.out.println("Digite o Id :");
                     int IdAgenda = scanner.nextInt();
-                    System.out.println(ServiceDB.selectDB(getContactbyId(IdAgenda)));
+                    System.out.println(ServiceDB.selectDBContact(getContactbyId(IdAgenda), connection).toString());
                     break;
                 case 6:
                     System.out.println("Digite o nome :");
                     String findNome = scanner.next();
-                    System.out.println(ServiceDB.selectDB(getContactByName(findNome)));
+                    System.out.println(ServiceDB.selectDBContact(getContactByName(findNome), connection).toString());
                     break;
                 case 9:
                     break;
@@ -62,11 +97,12 @@ public class Main {
 
     private static void getMenu() {
         System.out.println("1 - Inserir Contato");
-        System.out.println("2 - Inserir Phone");
-        System.out.println("3 - Inserir Grupo");
-        System.out.println("4 - Listar Contatos");
+        System.out.println("2 - Adicionar Telefone");
+        System.out.println("3 - Adcionar Grupo");
+        System.out.println("4 - Listar Todos Contatos");
         System.out.println("5 - Buscar contato por Id");
         System.out.println("6 - Buscar contato por Nome");
+        System.out.println("4 - Inserir Contatos no grupo");
         System.out.println("9 - Encerrar o programa");
     }
 }
